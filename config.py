56d1dce596106983635db154834b7bd664050ab6
TEAM TMM <alooneee772@gmail.com>
2025-02-01 19:49:51 +0545

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
 OWNER_ID = int(getenv("OWNER_ID", "-7414045632"))
 
-BOT_USERNAME = getenv("BOT_USERNAME" , "reesomusicbot)
+BOT_USERNAME = getenv("BOT_USERNAME" , "reesomusicbot")
 
 COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
 
